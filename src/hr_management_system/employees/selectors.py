4030dfe6391350335d26a0cd64a78f0017939f63
Faroogh <faroogh.oghba@gmail.com>
2025-02-26 15:58:52 +0100

    refactor(employees/selectors): update employee retrieval functions
    Refactored the employee retrieval functions to improve type hinting and
    clarity. The `get_employee` function now explicitly returns an
    optional `Employee` object based on the provided `employee_id`, while
    `get_employees` has been updated to return an optional `QuerySet`
    of `Employee` objects.
    
    No functionality has changed, but the return types are now more
    explicit, enhancing readability and maintainability.

--- src/hr_management_system/employees/selectors.py
+++ src/hr_management_system/employees/selectors.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from django_mongoengine import QuerySet
 from django_mongoengine.mongo_auth.managers import get_user_document
 
 from src.hr_management_system.core.exceptions import (
@@ -8,14 +9,21 @@ from src.hr_management_system.core.exceptions import (
 from src.hr_management_system.employees.models import Employee
 
 
-User = get_user_document()
-
-
-def get_employees() -> Optional['Employee']:
+def get_employee(*, employee_id: int) -> Optional['Employee']:
 
     """
 
     """
+    try:
+        return Employee.objects.get(employment_id=employee_id)
+    except Employee.DoesNotExist:
+        return None
 
+
+def get_employees() -> Optional[QuerySet['Employee']]:
+
+    """
+
+    """
 
     return Employee.objects()
